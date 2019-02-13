c4a85ebdd2865623eb413b2defba1c0d2c6ceded
alvinzt24 <alvinlistyo24@gmail.com>
2019-02-13 10:55:27 +0700

    Program saya

---
+++ program1.py
@@ -0,0 +1,11 @@
+a 	    = int(input("Masukan nilai a : "))
+b		= int(input("Masukan nilai b : "))
+c   	= int(input("Masukan nilai c : "))
+
+print("======================================")
+if a > b and a > c :
+	print("Nilai terbesar adalah :", a)
+elif b > a and  b > c :
+	print("Nilai terbesar adalah :", b)
+else :
+	print("Nilai terbesar adalah :", c)
